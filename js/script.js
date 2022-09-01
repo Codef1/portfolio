76579338798e8a1c391bbafed1815e7eac8a510c
farid yusuf <faridyusuf1@hotmail.com>
2022-09-01 22:11:28 +0300

    Add responsive menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const navbar = document.querySelector(".nav-bar");
 const menuItems = document.querySelectorAll(".menu-item");
+const burgerMenu = document.querySelector(".burger-menu");
 
 navbar.addEventListener("click", (e) => {
   const menuItem = e.target.closest(".menu-item");
   menuItems.forEach((el) => el.classList.remove("active"));
   menuItem.classList.add("active");
-  console.log(navbar.offsetHeight);
+  navbar.classList.remove("open");
 });
 
 document.addEventListener("scroll", (e) => {
@@ -14,4 +15,8 @@ document.addEventListener("scroll", (e) => {
   } else {
     navbar.classList.remove("scrolled");
   }
+});
+
+burgerMenu.addEventListener("click", () => {
+  navbar.classList.toggle("open");
 });
